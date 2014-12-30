3abaab45d553f391b7c698930e50624be9435218
liyachuan <applesstt@gmail.com>
2014-12-30 20:24:25 +0800

    validate on login and signup

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -36,6 +36,7 @@ exports.create = function (req, res) {
   user.provider = 'local';
   user.save(function (err) {
     if (err) {
+      console.log(err);
       return res.render('users/signup', {
         error: utils.errors(err.errors),
         user: user,
