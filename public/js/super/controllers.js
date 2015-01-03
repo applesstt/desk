3d27f557c6c240b952354fc9b49f3ea86549ba68
liyachuan <applesstt@gmail.com>
2015-01-03 15:52:27 +0800

    fetch base function from super controllors

--- public/js/super/controllers.js
+++ public/js/super/controllers.js
@@ -1,30 +1,24 @@
 'use strict';
 
+var _basePaginations = function(scope, resource) {
+  scope.wrapData = resource.get({});
+  scope.maxSize = 5;
+  scope.pageChanged = function() {
+    scope.wrapData = resource.get({
+      page: scope.wrapData.page,
+      perPage: scope.wrapData.perPage
+    });
+  }
+}
+
 /* Controllers */
 
 function UserCtrl($scope, SuperUsers) {
-  $scope.wrapData = SuperUsers.get({});
-
-  $scope.maxSize = 5;
-
-  $scope.pageChanged = function() {
-    $scope.wrapData = SuperUsers.get({
-      page: $scope.wrapData.page,
-      perPage: $scope.wrapData.perPage
-    });
-  }
-
+  _basePaginations($scope, SuperUsers);
 }
 
 function AdminCtrl($scope, SuperAdmins) {
-  $scope.wrapData = SuperAdmins.get({});
-  $scope.maxSize = 5;
-  $scope.pageChanged = function() {
-    $scope.wrapData = SuperAdmins.get({
-      page: $scope.wrapData.page,
-      perPage: $scope.wrapData.perPage
-    })
-  }
+  _basePaginations($scope, SuperAdmins);
 }
 
 function HomeArticlesCtrl($scope) {
@@ -36,25 +30,11 @@ function HomeStarsCtrl($scope) {
 }
 
 function ArticleCtrl($scope, SuperArticles, superFactory) {
-  $scope.wrapData = SuperArticles.get({});
-  $scope.maxSize = 5;
-  $scope.pageChanged = function() {
-    $scope.wrapData = SuperArticles.get({
-      page: $scope.wrapData.page,
-      perPage: $scope.wrapData.perPage
-    })
-  }
+  _basePaginations($scope, SuperArticles);
   $scope.hasBriefImg = superFactory.hasBriefImg;
 }
 
 function CommentCtrl($scope, SuperComments, superFactory) {
-  $scope.wrapData = SuperComments.get({});
-  $scope.maxSize = 5;
-  $scope.pageChanged = function() {
-    $scope.wrapData = SuperComments.get({
-      page: $scope.wrapData.page,
-      perPage: $scope.wrapData.perPage
-    })
-  }
+  _basePaginations($scope, SuperComments);
   $scope.hasBriefImg = superFactory.hasBriefImg;
 }
